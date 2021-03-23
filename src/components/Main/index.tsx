2790ecc61a0cb957b936fe96075693755f71f6dc
Thiago Soares <ths.pereira@gmail.com>
2021-03-23 20:38:30 -0300

    Atualização do boilerplate para projetos iniciais

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,10 +2,10 @@ import * as S from './styles'
 
 const Main = () => (
   <S.Wrapper>
-    <S.Logo src="/img/logo.svg" alt="Logo da AdebSystem" />
+    <S.Logo src="/img/reactjs-ar21.svg" alt="Logo do React - Next" />
+    <S.Title>App Boilerplate Next</S.Title>
+    <S.Description>Aplicativo inicial para projetos Reactjs/Next</S.Description>
     <S.Illustration src="/img/undraw_my_current_location_om7g.svg" />
-    <S.Title>Adebsystem</S.Title>
-    <S.Description>App para localização das igrejas</S.Description>
   </S.Wrapper>
 )
 
