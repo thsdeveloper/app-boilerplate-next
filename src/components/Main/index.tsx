341b19a35ed937954fa2f8dcc260913a523a8e8a
Thiago Pereira <ths.pereira@gmail.com>
2021-03-23 17:52:49 -0300

    Atualizações do bolierplate

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,12 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Logo da AdebSystem" />
+    <S.Illustration src="/img/undraw_my_current_location_om7g.svg" />
+    <S.Title>Adebsystem</S.Title>
+    <S.Description>App para localização das igrejas</S.Description>
+  </S.Wrapper>
 )
 
 export default Main
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -3,13 +3,19 @@ import { render, screen } from '@testing-library/react'
 import Main from '.'
 
 describe('<Main />', () => {
-  it('Deve renderizar o título', () => {
+  it('should render the heading', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /react avadddddddnçado/i })
+      screen.getByRole('heading', { name: /Adebsystem/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
