ea85084862e4d1ba83f6132302abd8d204853505
Thiago Pereira <ths.pereira@gmail.com>
2021-03-23 12:07:47 -0300

    task 124877 - inicial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
-export default function Home() {
+type Props = {
+  title: string
+}
+
+const bla = 'var'
+
+export default function Home({ title = 'Adebsystem Maps' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next App {title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js! {title}</a>
         </h1>
 
         <p className={styles.description}>
